07e773f6272143fb9ab788f47fdc758ed0743906
Muhammad <muhammad.mwd@gmail.com>
2022-08-06 12:26:00 +0500

    Finished styling

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,29 +1,39 @@
-import React from 'react'
-import home1 from '../img/home1.png'
+import React from "react";
+import home1 from "../img/home1.png";
+
+import { About, Description, Hide, Image} from "../styles";
 
 const AboutSection = () => {
-  return (
-    <div>
-        <div className="description">
-            <div className="title">
-                <div className="hide">
-                    <h2>We work to make</h2>
+    return (
+        <About>
+            <Description>
+                <div className="title">
+                    <Hide>
+                        <h2>We work to make</h2>
+                    </Hide>
+                    <Hide>
+                        <h2>
+                            your <span>dreams</span> come
+                        </h2>
+                    </Hide>
+                    <Hide>
+                        <h2>true.</h2>
+                    </Hide>
                 </div>
-                <div className="hide">
-                    <h2>your <span>dreams</span> come</h2>
-                </div>
-                <div className="hide">
-                    <h2>true.</h2>
-                </div>
-            </div>
-            <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</p>
-            <button>Contact Us</button>
-        </div>
-        <div className="image">
-            <img src={home1} alt="guy with a camera" />
-        </div>
-    </div>
-  )
-}
+                <p>
+                    Contact us for any photography or videography ideas that you
+                    have. We have professionals with amazing skills.
+                </p>
+                <button>Contact Us</button>
+            </Description>
+            <Image>
+                <img src={home1} alt="guy with a camera" />
+            </Image>
+        </About>
+    );
+};
 
-export default AboutSection
+//Styled Components
+
+
+export default AboutSection;
--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,8 +1,10 @@
 import React from "react";
+import styled from "styled-components";
+import { About } from "../styles";
 
 const FaqSection = () => {
     return (
-        <div className="faq">
+        <Faq>
             <h2>
                 Any questions <span>FAQ</span>
             </h2>
@@ -10,32 +12,81 @@ const FaqSection = () => {
                 <h4>How do I start?</h4>
                 <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Cconsectetur adipisicing elit. Quidem quod voluptatibus vero eos! Mollitia.</p>
+                    <p>
+                        Cconsectetur adipisicing elit. Quidem quod voluptatibus
+                        vero eos! Mollitia.
+                    </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Daily Schedule</h4>
                 <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Cconsectetur adipisicing elit. Quidem quod voluptatibus vero eos! Mollitia.</p>
+                    <p>
+                        Cconsectetur adipisicing elit. Quidem quod voluptatibus
+                        vero eos! Mollitia.
+                    </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Different Payment Methods</h4>
                 <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Cconsectetur adipisicing elit. Quidem quod voluptatibus vero eos! Mollitia.</p>
+                    <p>
+                        Cconsectetur adipisicing elit. Quidem quod voluptatibus
+                        vero eos! Mollitia.
+                    </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>What Products do you offer?</h4>
                 <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Cconsectetur adipisicing elit. Quidem quod voluptatibus vero eos! Mollitia.</p>
+                    <p>
+                        Cconsectetur adipisicing elit. Quidem quod voluptatibus
+                        vero eos! Mollitia.
+                    </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
-        </div>
+        </Faq>
     );
 };
+
+const Faq = styled(About)`
+    display: block;
+
+    span {
+        display: block;
+    }
+
+    h2 {
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+
+    .faq-line {
+        background-color: #ccc;
+        height: 0.2rem;
+        margin: 2rem 0;
+        width: 100%;
+    }
+
+    .question {
+        padding: 3rem 0;
+        cursor: pointer;
+    }
+
+    .answer {
+        padding: 2rem 0;
+
+        p {
+            padding: 1rem 0;
+        }
+    }
+`
 
 export default FaqSection;
